0dcf98b5813c831c2266f31220d5879036a6c8f1
Grayson <graysonpberger@gmail.com>
2019-07-30 05:02:00 -0400

    Add restful routes

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,30 @@
 class TaskController < ApplicationController
 
-    get '/tasks' do
+    get '/tasks' do #only if logged in, otherwise redirect to login page
+                    # should only display my task
+
+    end
+
+    post '/tasks' do
+
+    end
+
+    get '/tasks/new' do
+
+    end
+
+    get '/task/:id' do
+
+    end
+
+    get '/task/:id/edit' do
+    end
+
+    patch '/task/:id' do
+    end
+
+    delete '/task/:id' do
+        
     end
 
 end
